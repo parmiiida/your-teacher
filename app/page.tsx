83291626b1eeece38cbb6dc3a3ea8635106e5e2e
papichaa <parmida@danumgroup.com>
2025-08-17 19:36:51 +0300

    homepage implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div className='text-2xl underline'>
-      <h1>Page</h1>
-      <Button>Click me</Button>
-    </div>
-    
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          duration={45}
+          color="#FFDA6E"
+          subject="science"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={30}
+          color="#e5d0ff"
+          subject="Maths"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language"
+          duration={30}
+          color="#bde7ff"
+          subject=" English Literature"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          className="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
@@ -10,10 +11,11 @@ const Navbar = () => {
         <div className="flex items-center gap-2.5 cursor-pointer">
           <Image src="/images/logo.svg" width={46} height={46} alt="logo" />
         </div>
-        <div className="flex items-center gap-8">
-          <NavItems />
-        </div>
       </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <Button>Sign in</Button>
+      </div>
     </nav>
   );
 };

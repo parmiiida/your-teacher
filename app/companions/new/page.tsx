acc9aadfcea1b4fc56857445e1a07de2e5356da6
papichaa <parmida@danumgroup.com>
2025-08-18 18:54:29 +0300

    clerk setup done

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import CompanionForm from "@/components/CompanionForm";
+import React from "react";
 
 const NewCompanion = () => {
   return (
-    <div>
-      new 
-    </div>
-  )
-}
+    <main className="min-lg:w-1/3 items-center justify-center min-md:w-1/3">
+      <article className="w-full gap-4 flex flex-col">
+        <h1> Companion builder</h1>
 
-export default NewCompanion
+        <CompanionForm />
+      </article>
+    </main>
+  );
+};
+
+export default NewCompanion;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,18 +3,26 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-          <Image src="/images/logo.svg" width={46} height={46} alt="logo" />
+          <Image src="/images/logo.png" width={70} height={56} alt="logo" />
         </div>
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <Button>Sign in</Button>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
